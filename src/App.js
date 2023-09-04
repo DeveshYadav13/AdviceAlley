7347944310efbff9b1f24763bc8c373f9a3511d6
Devesh <deveshamway@gmail.com>
2023-09-05 04:15:38 +0530

    Props

--- src/App.js
+++ src/App.js
@@ -23,11 +23,17 @@ function App() {
     <div>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>Get more advice</button>
-      <p>
-        You have read <strong>{count}</strong> pieces of advice.
-      </p>
+      <Message count = {count} />          
     </div>
   );
+
+  function Message(props){
+    return(
+      <p>
+        You have read <strong>{props.count}</strong> pieces of advice.
+      </p>
+    );
+  }
 }
 
 export default App;
